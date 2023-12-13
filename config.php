19b9c1b0c7ff5b74db44c01ff94c3960a116521e
Ontiveros Alfaro Johán Alejandro <111895521+ChemsJam@users.noreply.github.com>
2023-12-13 16:05:58 -0600

    Practica de Crud en PHP

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+class Config{
+    const SERVER = "localhost";
+    const DB = "foro";
+    const USER = "root";
+    const PASS = "";
+
+    const SECRET = "Siiuuuuuu3l8ichom3555ixdxdxddddddddddddddddddddddddddddddddd";
+    
+}
